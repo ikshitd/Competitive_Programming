20ecfc5d2d4bae83adac54080f569fe2a86417c7
ikshitd <90301781+ikshitd@users.noreply.github.com>
2022-08-10 10:11:35 +0530

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+int partition(int arr[] , int l , int h){
+	int pivot = arr[h] ; 
+	int i = l - 1 ;
+	for(int j = l ; j < h  ;j++){
+		if(arr[j] < pivot){
+			i += 1 ; 
+			swap(arr[i] , arr[j]); 
+		}
+	}
+	swap(arr[i + 1] , arr[h]); 
+	return i + 1 ; 
+}
+void quicksort(int arr[] , int l , int h){
+	if(l >= h){
+		return ;
+	}
+	int p = partition(arr , l , h) ; 
+	quicksort(arr , l , p - 1 ); 
+	quicksort(arr , p + 1 , h) ; 
+}
+void solve() {
+	int n ; 
+	cin >> n ; 
+	int arr[n] ; 
+	for(int i = 0 ;i < n ;i++){
+		cin >> arr[i] ; 
+	}
+	int l = 0 , h = n - 1 ; 
+	quicksort(arr , l , h) ; 
+	print(arr) ; 
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+}
